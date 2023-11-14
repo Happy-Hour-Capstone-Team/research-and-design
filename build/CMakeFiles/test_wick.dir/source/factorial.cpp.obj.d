2d55cc68164d3012b6b141d683826616ed41a77b
theguy20phi <46367025+theguy20phi@users.noreply.github.com>
2023-11-14 15:43:32 -0600

    Remove build.

--- build/CMakeFiles/test_wick.dir/source/factorial.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_wick.dir/source/factorial.cpp.obj: \
- C:/Users/2020s/Documents/GitHub/wick-interpreter/source/factorial.cpp \
- C:/Users/2020s/Documents/GitHub/wick-interpreter/include/factorial.hpp
