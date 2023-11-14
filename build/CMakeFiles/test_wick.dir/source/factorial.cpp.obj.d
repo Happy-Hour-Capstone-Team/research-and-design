d4ec83a6ed8a615bc9edb5b8e090a06b71d8c6e0
lissetluna <122905807+lissetluna@users.noreply.github.com>
2023-11-14 15:42:14 -0600

    build file deleted

--- build/CMakeFiles/test_wick.dir/source/factorial.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_wick.dir/source/factorial.cpp.obj: \
- C:/Users/lisse/Documents/GitHub/wick-interpreter/source/factorial.cpp \
- C:/Users/lisse/Documents/GitHub/wick-interpreter/include/factorial.hpp
