fbb0952b34c6521f79cc66f1b5970e6624fed6f0
theguy20phi <bradenwepierce@gmail.com>
2024-01-27 18:59:24 -0600

    Write out expected desired grammar.

--- source/main.cpp
+++ source/main.cpp
@@ -6,20 +6,18 @@
  * STATEMENT -> EXPRESSION_STATEMENT | BLOCK | IF | WHILE
  * EXPRESSION_STATEMENT -> EXPRESSION ;
  * BLOCK -> ( begin DECLARATION* end ) | ( { DECLARATION* } )
- * IF -> if ( EXPRESSION ) STATEMENT ( else STATEMENT )?
- * WHILE_STATEMENT -> while ( EXPRESSION ) STATEMENT
- * EXPRESSION ->
+ * IF -> if '(' EXPRESSION ')' STATEMENT ( else STATEMENT )?
+ * WHILE_STATEMENT -> while '(' EXPRESSION ')' STATEMENT
+ * EXPRESSION -> OR
+ * OR -> AND ( or AND )*
+ * AND -> EQUALITY ( and EQUALITY )*
+ * EQUALITY -> COMPARISON ( ( == | != ) COMPARISON )*
+ * COMPARISON -> TERM ( ( < | <= | > | >= ) TERM )*
+ * TERM -> FACTOR ( ( + | - ) FACTOR )*
+ * FACTOR -> UNARY ( ( * | / ) UNARY )*
+ * UNARY -> ( ! | - ) PRIMARY
+ * PRIMARY -> true | false | Number | String | Identifier | '( EXPRESSION ')'
  */
-
-// equality -> expression
-class Expression {};
-
-struct Equality {
-  Comparison comparison;
-};
-class Parser {
-  public:
-};
 
 int main() {
   return 0;
