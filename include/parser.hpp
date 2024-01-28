2a33c30fdb71b84d9a2d558f6547fe3da22e3368
theguy20phi <bradenwepierce@gmail.com>
2024-01-28 01:56:50 -0600

    Begin laying out grammar.

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,18 +1,1 @@
 #pragma once
-#include <string>
-#include <vector>
-
-enum class TokenType {
-  Identifier,
-  Integer,
-  Real,
-  LeftParenthesis,
-  Equals,
-  StatementEnd,
-  Custom
-};
-
-struct Token {
-  TokenType type;
-  std::string lexeme;
-};
--- source/main.cpp
+++ source/main.cpp
@@ -18,7 +18,72 @@
  * PRIMARY -> true | false | Number | String | Identifier | '( EXPRESSION ')'
  */
 
-class Parser {
+// Forward declares in order to declare visit methods.
+#include <memory>
+
+struct Literal;
+struct Unary;
+struct Binary;
+struct Group;
+
+struct Expression {
+  class Visitor {
+    public:
+    virtual Token visit(const Literal &literal) = 0;
+    virtual Token visit(const Unary &unary) = 0;
+    virtual Token visit(const Binary &binary) = 0;
+    virtual Token visit(const Group &group) = 0;
+  };
+
+  virtual Token accept(const std::unique_ptr<Visitor> visitor) = 0;
+};
+
+struct Literal : Expression {
+  Literal(const Token &iToken) : token{iToken} {}
+  const Token token;
+
+  Token accept(const std::unique_ptr<Visitor> visitor) override {
+    return visitor->visit(*this);
+  }
+};
+
+struct Unary : Expression {
+  Unary(const Token &iOp, std::unique_ptr<Expression> iRight) :
+      op{iOp},
+      right{std::move(iRight)} {}
+  const Token op;
+  const std::unique_ptr<Expression> right;
+
+  Token accept(const std::unique_ptr<Visitor> visitor) override {
+    return visitor->visit(*this);
+  }
+};
+
+struct Binary : Expression {
+  Binary(std::unique_ptr<Expression> iLeft,
+         const Token &iOp,
+         std::unique_ptr<Expression> iRight) :
+      left{std::move(iLeft)},
+      op{iOp},
+      right{std::move(iRight)} {}
+  const std::unique_ptr<Expression> left;
+  const Token op;
+  const std::unique_ptr<Expression> right;
+
+  Token accept(const std::unique_ptr<Visitor> visitor) override {
+    return visitor->visit(*this);
+  }
+};
+
+struct Group : Expression {
+  const std::unique_ptr<Expression> expression;
+
+  Token accept(const std::unique_ptr<Visitor> visitor) override {
+    return visitor->visit(*this);
+  }
+};
+
+struct Parser {
   public:
   private:
   Tokens tokens;
@@ -26,5 +91,12 @@ class Parser {
 };
 
 int main() {
+  std::unique_ptr<Expression> test = std::make_unique<Binary>(
+      std::make_unique<Unary>(
+          Token{"!", Token::Type::Exclamation},
+          std::make_unique<Literal>(Token{"true", Token::Type::Boolean})),
+      Token{"or", Token::Type::Or},
+      std::make_unique<Literal>(Token{"true", Token::Type::Boolean}));
+
   return 0;
 }
